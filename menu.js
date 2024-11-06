43ab464621d72d18317a0316f093d0c8c2c9567a
amandamuniz-design <amandamuniz.design@gmail.com>
2024-11-06 15:53:20 -0300

    aut

---
+++ menu.js
@@ -0,0 +1,7 @@
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('burguer')
+
+btnMenu.addEventListener('click',()=>{
+    menu.classList.add('abrir-menu')
+})
