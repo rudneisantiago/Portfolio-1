fefa1731b9c469e2b15132cb288ecff598bf2712
Rudnei Santiago <rudnei.santiago@gmail.com>
2024-11-26 21:48:25 -0300

    Ajuste no menu

--- menu.js
+++ menu.js
@@ -1,7 +1,17 @@
+let btnMenu = document.getElementById("btn-menu");
+let menu = document.getElementById("burguer");
+let fechar = document.getElementById("botaofechar");
 
-let btnMenu = document.getElementById('btn-menu')
-let menu = document.getElementById('burguer')
+btnMenu.addEventListener("click", () => {
+  menu.classList.add("abrir-menu");
+  btnMenu.classList.add("esconder-botao");
+});
 
-btnMenu.addEventListener('click',()=>{
-    menu.classList.add('abrir-menu')
-})
+fechar.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
+  btnMenu.classList.remove("esconder-botao");
+});
+
+let p = document.createElement("p");
+p.innerText = "Oi pessoal";
+menu.append(p);
